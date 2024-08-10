c99d4e083aa43f7012dbf524ea21b822f7bac5d0
Valeryio <tommagoma23@gmail.com>
2024-08-10 15:54:45 +0100

    First draft of the footer
    The footer have been edited with its css code, and that's matching the
    design!

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -8,9 +8,6 @@ let hambugerMenu = document.querySelector(".header__hamburger-menu");
 hambugerMenu.addEventListener("click", (e) =>{
 
     headerCategoriesContainer.classList.toggle("show-flexbox");
-
     headerNavlistContact.classList.toggle("show-flexbox")
     headerNavlist.classList.toggle("show-flexbox")
-
-    console.log("clicked")
 });
